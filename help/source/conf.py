7eb4dd1a2acec862f13d687a7ddb82683fed9d93
jonah <geosull@pm.me>
2026-04-25 16:15:42 +1000

    update docs

--- help/source/conf.py
+++ help/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'QGIS Plugin Builder'
-copyright = u'Copyright 2011-2026, Gary Sherman, 2026 Jonah Sullivan'
+copyright = u'2011-2026, Gary Sherman, 2026 Jonah Sullivan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
